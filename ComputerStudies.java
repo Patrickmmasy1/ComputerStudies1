53ca802cc6d7108b15d45185ab49a72aa7752eb9
Patrickmmasy1 <168182837+Patrickmmasy1@users.noreply.github.com>
2024-04-26 16:01:23 +0300

    Add files via upload

---
+++ ComputerStudies.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package computerstudies;
+
+/**
+ *
+ * @author PC
+ */
+public class ComputerStudies {
+   
+    public void Display(){
+        
+    }
+    
+     public void module (){
+     }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                System.out.print("Module Name: Object Oriented \n");
+                System.out.print("Module Code: CSEU 07205 \n");
+                System.out.print("Module Instructor Name: Mwinuka \n");
+                
+
+    }
+    
+}
